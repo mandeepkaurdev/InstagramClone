9f55f58928cc92ce48f23916a1e1bef43d6dcbb6
julianamtyler <julianam.tyler@gmail.com>
2018-10-07 19:06:53 -0400

    edited my app_comments.js

--- public/app_comments.js
+++ public/app_comments.js
@@ -4,11 +4,8 @@ $(function () {
         const allComments = function () {
             $('#allComments').empty();
             $.ajax({ url: '/api/instagram', method: 'GET' })
-                .then(function (newComment) {
-                    newComment.forEach(ele => {
-                        $('p').html();
-                    });
-                // console.log('new comment')
+                .then(function (allComments) {
+                console.log('allComments')
 
                     })
                 }
@@ -18,8 +15,7 @@ $(function () {
         const postItem = function (newComment) {
             $.ajax({ url: '/api/instagram', method: 'POST', data: newComment })
                 .then(function (comments) {
-                let commentname = $('#newComment').val();
-                    $('#allComments').append($($,{commentname}))
+                
                 })
     
     
