19463720e76c3c6dcf7e3fc142f24e10cb7c11bb
julianamtyler <julianam.tyler@gmail.com>
2018-10-07 14:51:57 -0400

    created models and added input boxes on html for comment input

--- models/comments.js
+++ models/comments.js
@@ -5,12 +5,15 @@ const Schema = mongoose.Schema;
 const commentsSchema = new Schema({
 
   comments: [{
-    type: String,
+    type: Schema.Types.ObjectId,
+    ref: "eachComment",
     trim: true,
-  }]
 
+  }]
+  
 });
 
 var comments = mongoose.model("comments", commentsSchema);
 
 module.exports = comments;
+
