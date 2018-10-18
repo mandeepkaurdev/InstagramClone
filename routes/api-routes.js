f537e18eb8e9bd72f0ba0c86be92f3e779802ecc
julianamtyler <julianam.tyler@gmail.com>
2018-10-18 18:56:41 -0400

    fixed heart stay

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,22 +5,22 @@ const multer = require('multer');
 const fs = require('fs');
 const path = require('path')
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
+    destination: function (req, file, cb) {
         cb(null, 'public/myPicFolder');
     },
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + file.originalname);   
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
     }
 });
-const upload = multer({storage: storage});
+const upload = multer({ storage: storage });
 
 module.exports = function (app) {
 
     app.get('/api/photos', function (req, res) {
-        db.photos.find({})      
-            .sort({photo_url: -1})
-            .populate("likes","likes")
-            .populate('comments') 
+        db.photos.find({})
+            .sort({ photo_url: -1 })
+            .populate("likes", "likes")
+            .populate('comments')
             .then(function (photos) {
                 res.json(photos);
             })
@@ -30,25 +30,25 @@ module.exports = function (app) {
     });
 
     app.post('/api/photo', upload.single('inputUploadPhoto'), function (req, res, next) {
-        db.photos.create({"photo_url": (req.file.path).replace('public', '')})
+        db.photos.create({ "photo_url": (req.file.path).replace('public', '') })
             .then(function (photos) {
                 res.redirect(req.protocol + '://' + req.get('host'));
             })
             .catch(function (err) {
                 res.json(err);
             });
-            
+
     });
 
     app.delete('/api/photo/:index', function (req, res) {
-        db.photos.findByIdAndDelete({_id: req.params.index})
+        db.photos.findByIdAndDelete({ _id: req.params.index })
             .then(function (photo) {
-                let imgPathToDelete = "./public/myPicFolder/" + photo.photo_url.replace("\\myPicFolder\\","");
+                let imgPathToDelete = "./public/myPicFolder/" + photo.photo_url.replace("\\myPicFolder\\", "");
                 fs.unlink(imgPathToDelete, (err) => {
                     if (err) {
-                        console.log("failed to delete local image:"+err);
+                        console.log("failed to delete local image:" + err);
                     } else {
-                        console.log('successfully deleted local image');                                
+                        console.log('successfully deleted local image');
                     }
                 });
                 res.json(photos);
@@ -58,18 +58,21 @@ module.exports = function (app) {
             });
     });
 
-// gina code ends
+    // gina code ends
 
     app.post('/api/likes', function (req, res) {
-        db.likes.create(req.body)
-            .then(function (likes) {
-                res.json(likes);
+        console.log(req.body);
+        db.likes.create({ likes: req.body.isLiked })
+            .then(function (dblike) {
+                return db.photos.findOneAndUpdate({ _id: req.body._id }, { $push: { likes: dblike._id } }, { new: true });
+            })
+            .then(function (dbPhoto) {
+                res.json(dbPhoto);
             })
             .catch(function (err) {
                 res.json(err);
             });
     });
-
 
 
     app.post('/api/comments', function (req, res) {
@@ -78,13 +81,13 @@ module.exports = function (app) {
              userComment: "I like this photo",
              photo_id: '15675645647647.jpg'
         }*/
-        db.eachComment.create({userComment: req.body.userComment})
-        .then(function (comments) {
-            return db.photos.findOneAndUpdate({_id: req.body.photo_id}, { $push: { comments: comments._id } }, { new: true })
+        db.eachComment.create({ userComment: req.body.userComment })
+            .then(function (comments) {
+                return db.photos.findOneAndUpdate({ _id: req.body.photo_id }, { $push: { comments: comments._id } }, { new: true })
 
             })
-            
-            .then(function (photos){
+
+            .then(function (photos) {
                 res.json(photos)
             })
             .catch(function (err) {
@@ -104,7 +107,7 @@ module.exports = function (app) {
 
     app.get('/api/comments', function (req, res) {
         db.eachComment.find({})
-           
+
             .then(function (comments) {
                 res.json(comments);
             })
